8ecb650ab0feba75fa522a957c8335439a8feee9
taeryu7 <taeryu97@gmail.com>
2024-12-03 19:13:50 +0900

    C,R

--- CoreDataStudy/CoreDataStudy/AppDelegate.swift
+++ CoreDataStudy/CoreDataStudy/AppDelegate.swift
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: - Core Data stack
-
+    // 원래는 없는 메서드지만 coredata를 추가해서 자동생성된 메서드
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -41,6 +41,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
+        
+        // - `NSPersistentContainer` 는 CoreData에서 데이터를 저장하고 관리하는 데 필요한 핵심 객체. → 직역해보면 영구적인 저장 장소.
+        
+        // 먼저 NSPersistentContainer 를 생성해줘야하는데, 프로젝트 생성할 때 CoreData 를 사용한다고 체크 해줬으므로,
+        // AppDelegate.swift 에 기본적으로 NSPersistentContainer 를 세팅하는 코드가 존재.
+        
+        // NSPersistentConatiner 를 만들었으므로, ViewController 에서 이를 활용해서 데이터 접근.
         let container = NSPersistentContainer(name: "CoreDataStudy")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
@@ -62,7 +69,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     // MARK: - Core Data Saving support
-
+    // 원래는 없는 메서드지만 coredata를 추가해서 자동생성된 메서드
+    
+    // AppDelegate.swift 아래쪽에보면 saveContext() 라는 메서드도 자동으로 생성되어있는데, 직역하면 문맥을 저장한다는 뜻.
+    // 데이터의 업데이트(추가, 업데이트, 삭제 등)가 일어났으면 saveContext() 를 호출해서 그 문맥을 저장해야 함.
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
--- CoreDataStudy/CoreDataStudy/ViewController.swift
+++ CoreDataStudy/CoreDataStudy/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    var container: NSPersistentContainer! // ! 마크는 반드시 존재한다는 의미를 부여(추후 찾아볼 내용)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate // AppDelegate에 접근하기 위한 코드. UIApplication.shared.delegate : 기본제공하는 코프로퍼티
+                                                                        // 뒤의 as는 AppDelegate를 타입캐스팅 하기 위해서 사용, 타입캐스팅을 하면 폴더의 AppDelegate에 접근가능
+        self.container = appDelegate.persistentContainer
+        
+        createData(name: "Adam", phoneNumber: "010-1111-2222")
+        readAllData()
+        
     }
 
-
+    // CoreDataStudy 에 데이터 Create.
+    func createData(name: String, phoneNumber: String) {
+        guard let entity = NSEntityDescription.entity(forEntityName: "PhoneBook", in: self.container.viewContext) else { return }
+        let newPhoneBook = NSManagedObject(entity: entity, insertInto: self.container.viewContext)
+        newPhoneBook.setValue(name, forKey: "name")
+        newPhoneBook.setValue(phoneNumber, forKey: "phoneNumber")
+        
+        do {
+            try self.container.viewContext.save()
+            print("문맥 저장 성공")
+        } catch {
+            print("문맥 저장 실패")
+        }
+    }
+    
+    // CoreDataStudy 에서 데이터 Read.
+    func readAllData() {
+        do {
+            let phoneBooks = try self.container.viewContext.fetch(PhoneBook.fetchRequest())
+            
+            for phoneBook in phoneBooks as [NSManagedObject] {
+                if let name = phoneBook.value(forKey: "name") as? String,
+                   let phoneNumber = phoneBook.value(forKey: "phoneNumber") as? String {
+                    print("name: \(name), phoneNumber: \(phoneNumber)")
+                }
+            }
+            
+        } catch {
+            print("데이터 읽기 실패")
+        }
+    }
 }
 
